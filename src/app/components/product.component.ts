48ef351913d9e8f6b4d4f3a0f782dabb0ec20927
samRgarcia <samuelrivas18@gmail.com>
2020-11-26 19:09:49 -0600

    was added the page 404

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,42 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls:['./product.component.scss']
+})
+export class ProductComponent implements OnChanges,OnInit {
+  @Input() product: Product;
+  @Output() productCliked: EventEmitter<any> = new EventEmitter();
+
+  today = new Date();
+
+  constructor() {
+    console.log('constructor :)');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChange');
+    console.log(changes);
+  }
+
+  ngOnInit(){
+      console.log(' ngOnInit')
+  }
+
+
+
+
+  addCart() {
+    console.log('i am methods');
+    this.productCliked.emit(this.product.id);
+  }
+}
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {ProductsService} from './../products.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor(
+    private route:ActivatedRoute,
+    private productsService:ProductsService
+    ) {
+    
+   }
+
+  ngOnInit(){
+    this.route.params.subscribe((params:Params)=>{
+
+      const id = params.id;
+   const productss = this.productsService.getProduct(id);
+      console.info(productss);
+    })
+  }
+
+}
